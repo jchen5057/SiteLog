04125d6e1bb063651a1a9be1ded57e9324c2a1cc
Jorge Vergara <javebratt@gmail.com>
2017-04-06 14:56:08 -0500

    Removing duplicate code from module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -15,22 +15,6 @@ import { SignupPage } from '../pages/signup/signup';
 
 // Import providers
 import { AppProviders } from './app.providers';
-import { AuthData } from '../providers/auth-data';
-import { EventData } from '../providers/event-data';
-import { ProfileData } from '../providers/profile-data';
-
-// Import Plugin providers.
-import { Camera } from '@ionic-native/camera';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
-class CameraMock extends Camera {
-  getPicture(options){
-    return new Promise( (resolve, reject) => {
-      resolve("TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=");
-    });
-  }
-}
 
 @NgModule({
   declarations: [
