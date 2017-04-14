44fca02e5f6c3b0f552cdb717fe8e6ee93b72f50
Jorge Vergara <javebratt@gmail.com>
2017-04-14 12:25:31 -0500

    Mocking Camera Plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,10 @@ import { ProfileProvider } from '../providers/profile/profile';
 class CameraMock extends Camera {
   getPicture(options){
     return new Promise( (resolve, reject) => {
-      resolve("ADD_BASE64_STRING_RESPONSE_HERE");
+      resolve(`TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1
+      bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgY
+      SBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb2
+      4gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=`);
     });
   }
 }
